7c762e3c3527213b8a831f32c83a737e9084b695
RolandoQuintanaTPG <107889590+RolandoQuintanaTPG@users.noreply.github.com>
2022-10-06 16:12:09 -0600

    changed RF IO names

--- ALUandRF/RegFile.v
+++ ALUandRF/RegFile.v
@@ -4,9 +4,9 @@
 module regfile #(parameter WIDTH = 16, REGBITS = 4)
                 (input                clk, 
                  input                regwrite, 
-                 input  [REGBITS-1:0] ra1, ra2, wa, 
-                 input  [WIDTH-1:0]   wd, 
-                 output [WIDTH-1:0]   rd1, rd2);
+                 input  [REGBITS-1:0] regAddr1, regAddr2, wrAddr, 
+                 input  [WIDTH-1:0]   wrData, 
+                 output [WIDTH-1:0]   readData1, readData2);
 
    reg  [WIDTH-1:0] RAM [(1<<REGBITS)-1:0];
 	
@@ -20,10 +20,10 @@ module regfile #(parameter WIDTH = 16, REGBITS = 4)
    // dual-ported register file
    //   read two ports combinationally
    //   write third port on rising edge of clock
-   always @(posedge clk)
-      if (regwrite) RAM[wa] <= wd;
+   alwrAddrys @(posedge clk)
+      if (regwrite) RAM[wrAddr] <= wd;
 	
    // register 0 is hardwired to 0
-   assign rd1 = ra1 ? RAM[ra1] : 0;
-   assign rd2 = ra2 ? RAM[ra2] : 0;
+   assign readData1 = regAddr1 ? RAM[regAddr1] : 0;
+   assign readData2 = regAddr2 ? RAM[regAddr2] : 0;
 endmodule
