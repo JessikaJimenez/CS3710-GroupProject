387ea9f4f0cb3a09c15a065a51b523170d81172c
RolandoQuintanaTPG <107889590+RolandoQuintanaTPG@users.noreply.github.com>
2022-10-11 11:36:16 -0600

    removed flags from regfile

--- ALUandRF/RegFile.v
+++ ALUandRF/RegFile.v
@@ -3,12 +3,14 @@
 
 module regfile #(parameter WIDTH = 16, REGBITS = 4)
                 (input                clk, 
-                 input                regWrite, 
-                 input  [REGBITS-1:0] sourceAddr, destAddr, 
+                 input                regWrite,
+                 input  [WIDTH-1:0]   flags,
+                 input  [REGBITS-1:0] sourceAddr, destAddr,
                  input  [WIDTH-1:0]   wrData, 
                  output [WIDTH-1:0]   readData1, readData2);
 
    reg  [WIDTH-1:0] RAM [(1<<REGBITS)-1:0];
+   localparam conditionAddr = 4'b1111;
 	
 	initial begin
 	$display("Loading register file");
@@ -20,8 +22,18 @@ module regfile #(parameter WIDTH = 16, REGBITS = 4)
    // dual-ported register file
    //   read two ports combinationally
    //   write third port on rising edge of clock
-   always @(posedge clk)
+   always @(negedge reset, posedge clk) begin
+      if(~reset) begin
+        	$display("Loading register file");
+	      // you'll need to change the path to this file! 
+	      $readmemb("C:/Users/danie/Documents/Homework/22 Fall/ECE 3710/Quartus/MiniMips/reg.dat", RAM); 
+	      $display("done with RF load"); 
+      end
+
       if (regwrite) RAM[destAddr] <= wrData;
+      RAM[conditionAddr] <= flags;
+   end
+      
 	
    // register 0 is hardwired to 0
    assign readData1 = regAddr1 ? RAM[regAddr1] : 0;
