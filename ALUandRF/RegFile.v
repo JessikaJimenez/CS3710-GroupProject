d5d8f4f7f9f5e32ed3046cee8729553704816957
Jessikeks <jessikune@gmail.com>
2022-10-10 23:01:00 -0600

    Update RegFile.v
    Added variable for flags

--- ALUandRF/RegFile.v
+++ ALUandRF/RegFile.v
@@ -4,6 +4,7 @@
 module regfile #(parameter WIDTH = 16, REGBITS = 4)
                 (input                clk, 
                  input                regWrite, 
+		 input  	      flags,  //**TODO - fix this to have proper size and parameters
                  input  [REGBITS-1:0] sourceAddr, destAddr, 
                  input  [WIDTH-1:0]   wrData, 
                  output [WIDTH-1:0]   readData1, readData2);
