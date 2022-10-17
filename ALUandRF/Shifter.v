566968de063dc5990df3f6480e14748ca8f123c2
Nami Eskandarian <u1117098@utah.edu>
2022-10-17 15:32:57 -0600

    Updated Shifter module

--- ALUandRF/Shifter.v
+++ ALUandRF/Shifter.v
@@ -1,11 +1,11 @@
 // SHIFTER MODULE
 /*************************************************************/
 // This module will shift an input a desired amount of bits
-module Shifter #(parameter INPUTWIDTH = 16, parameter OUTPUTWIDTH = 16) 
+module Shifter #(parameter INPUTWIDTH = 16, parameter OUTPUTWIDTH = 16, parameter SHIFTWIDTH = 4) 
     (reset, shiftInput, shiftAmount, rightShift, shiftResult);
     input reset;
     input [INPUTWIDTH - 1 : 0] shiftInput;
-    input [3 : 0] shiftAmount;
+    input [SHIFTWIDTH - 1 : 0] shiftAmount;
     input rightShift; // Flag for determining left or right shift
     output reg [OUTPUTWIDTH - 1 : 0] shiftResult;
 
