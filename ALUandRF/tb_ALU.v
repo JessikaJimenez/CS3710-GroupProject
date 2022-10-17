c0ccfc3fd92fff067a41e2b243d6370ff2c4be2b
battl <battlecast33@gmail.com>
2022-10-17 20:39:38 +0000

    added some flag tests to tb_ALU

--- ALUandRF/tb_ALU.v
+++ ALUandRF/tb_ALU.v
@@ -33,7 +33,7 @@ module tb_ALU #(parameter WIDTH = 16) ();
        if (aluResult == 16'd2) $display("Add is correct (%d + %d = %d).", regDst, regSrc, aluResult);
        aluOp <= 3'b100;
        #10
-       if (aluResult == 16'd0) $display("Subtract is correct (%d - %d = %d)", regDst, regSrc, aluResult);
+       if (aluResult == 16'd0) $display("Subtract is correct (%d - %d = %d).", regDst, regSrc, aluResult);
        regSrc <= 16'd15;
        #10
        aluOp <= 3'b001;
@@ -45,6 +45,18 @@ module tb_ALU #(parameter WIDTH = 16) ();
        aluOp <= 3'b011;
        #10
        if (aluResult == 16'd14) $display("Xor is correct (%d ^ %d = %d).", regDst, regSrc, aluResult);
+	   aluOp <= 3'b100;
+	   regDst <= 16'b0000000000000011;
+	   regSrc <= 16'b0000000000000101;
+	   #10
+	   if (carry = 1) $display("carry flag is correct");
+	   else $display("something is wrong");
+	   aluOp <= 3'b000;
+	   regDst <= 16'b0000000000000011;
+	   regSrc <= 16'b0111111111111101;
+	   #10
+	   if (carry = 0 && flag = 1) $display("carry/flag flags are correct");
+	   else $display("something is wrong");
 	end
 	
 endmodule 
