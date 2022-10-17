a0bcfeb81f3a73c36faa4d2438f4614ef6d3619c
RolandoQuintanaTPG <107889590+RolandoQuintanaTPG@users.noreply.github.com>
2022-10-17 17:40:40 -0600

    Updated comments

--- ALUandRF/PSR.v
+++ ALUandRF/PSR.v
@@ -1,4 +1,4 @@
-// REGISTER FILE MODULE
+// PSR MODULE
 /*************************************************************/
 
 module PSR #(parameter WIDTH = 16) (
@@ -10,17 +10,14 @@ module PSR #(parameter WIDTH = 16) (
        reg  [WIDTH-1:0] RAM; //Register file is 16 bits wide and 1 register deep
 	
        initial begin
-          RAM <= 16'd0;
+          RAM <= 16'd0; //Clear the register
        end
 
-      // dual-ported register file
-      //   read two ports combinationally
-      //   write third port on rising edge of clock
       always @(negedge reset, posedge clk) begin
          if(~reset) begin
-            RAM <= 16'd0; 
+            RAM <= 16'd0; //Clear the register
          end
-			else RAM[WIDTH-1:0] <= flags; //Fill the register
+			else RAM[WIDTH-1:0] <= flags; //Fill the register with input flags
       end
       
        assign readFlags = RAM[WIDTH-1:0]; // assign the output
