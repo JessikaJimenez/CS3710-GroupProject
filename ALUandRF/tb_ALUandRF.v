879f6ae043e38bd310ae93a822776b0f68b63d36
Nami Eskandarian <u1117098@utah.edu>
2022-10-17 15:48:05 -0600

    Finished top module testbench and simulation

--- ALUandRF/tb_ALUandRF.v
+++ ALUandRF/tb_ALUandRF.v
@@ -8,7 +8,7 @@ module tb_ALUandRF #(parameter WIDTH = 16) ();
 	reg [WIDTH - 1 : 0] pc, immd;
 	reg regWrite;
 	wire [3:0] srcAddr, dstAddr;
-	wire pcInstruction, rTypeInstruction, shiftInstruction, flagSet;
+	wire pcInstruction, rTypeInstruction, shiftInstruction, flagSet, copyInstruction;
 	wire [2:0] aluOp;
 	wire [WIDTH - 1 : 0] resultData;
 	wire [WIDTH - 1 : 0] outputFlags;
@@ -25,6 +25,7 @@ module tb_ALUandRF #(parameter WIDTH = 16) ();
 		.rTypeInstruction(rTypeInstruction), 
 		.shiftInstruction(shiftInstruction), 
 		.flagSet(flagSet),
+		.copyInstruction(copyInstruction),
 		.regWrite(regWrite),
 		.aluOp(aluOp),
 		.resultData(resultData),
@@ -45,6 +46,7 @@ module tb_ALUandRF #(parameter WIDTH = 16) ();
 	assign opCode = rTypeInstruction ? instruction[7:4]:instruction[15:12];
 	assign aluOp = (opCode[3:2] == 2'b00) ? {1'b0, opCode[1:0]}:{opCode[3], 2'b00};
 	assign flagSet = (aluOp[1:0] == 2'b00);
+	assign copyInstruction = (opCode == 4'b1101);
 	assign carry = outputFlags[0];
 	assign low = outputFlags[1];
 	assign flag = outputFlags[2];
@@ -93,6 +95,72 @@ module tb_ALUandRF #(parameter WIDTH = 16) ();
 		#20
 		regWrite <= 0;
 		if (resultData == 16'd8) $display("$2 now equals 8 ($2 >> 1).");
+		instruction <= 16'b0011000100000001; // I-Type Xor ($1 ^ 1)
+		#50
+		regWrite <= 1;
+		#10
+		regWrite <= 0;
+		if (resultData == 16'd1) $display("$1 now equals 1 ($1 ^ 1).");
+		instruction <= 16'b0000001111010001; // R-Type Mov ($3 = $1)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd1) $display("$3 now equals 1 ($3 = $1).");
+		instruction <= 16'b1001001000000101; // I-Type Sub ($2 -= 5)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd3) $display("$2 now equals 3 ($2 -= 5).");
+		instruction <= 16'b1101010000001111; // I-Type Mov ($4 = 15)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd15) $display("$4 now equals 15 ($4 = 15).");
+		instruction <= 16'b0000010000010010; // R-Type And ($4 & $2)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd3) $display("$4 now equals 3 ($4 & $2).");
+		instruction <= 16'b0001010000000110; // I-Type And ($4 & 6)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd2) $display("$4 now equals 2 ($4 & 6).");
+		instruction <= 16'b0000010000100011; // R-Type Or ($4 | $3)
+		#50
+		regWrite <= 1;
+		#20
+		regWrite <= 0;
+		if (resultData == 16'd3) $display("$4 now equals 3 ($4 | $3).");
+		instruction <= 16'b0000010000110010; // R-Type Xor ($4 ^ $2)
+		#50
+		regWrite <= 1;
+		#10
+		regWrite <= 0;
+		if (resultData == 16'd0) $display("$4 now equals 0 ($4 ^ $2).");
+		instruction <= 16'b1000001001000001; // R-Type LSH ($2 << $1)
+		#50
+		regWrite <= 1;
+		#10
+		regWrite <= 0;
+		if (resultData == 16'd6) $display("$2 now equals 6 ($2 << $1).");
+		instruction <= 16'b1000001001100100; // R-Type ASHU ($2 << $4)
+		#50
+		regWrite <= 1;
+		#10
+		regWrite <= 0;
+		if (resultData == 16'd6) $display("$2 now equals 6 ($2 >> $4).");
+		instruction <= 16'b1000001000110010; // I-Type ASHU ($2 >> 2)
+		#50
+		regWrite <= 1;
+		#10
+		regWrite <= 0;
+		if (resultData == 16'd1) $display("$2 now equals 1 ($2 >> 2).");
 	end
 		
 	// Generate clock
