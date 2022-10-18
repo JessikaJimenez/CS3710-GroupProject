57a0789ccf260deb6af75ac1dee7c21d9e735261
Nami Eskandarian <u1117098@utah.edu>
2022-10-18 17:00:44 -0600

    Implemented PC flip-flop and MUX

--- Datapath/datapath.v
+++ Datapath/datapath.v
@@ -1,14 +1,38 @@
 // DATAPATH MODULE
 /*************************************************************/
-module datapath();
+module datapath #(parameter WIDTH = 16) ();
     input clk, reset;
+    input pcInstruction, rTypeInstruction, shiftInstruction, regWrite, flagSet, copyInstruction;
+    input [2:0] aluOp;
+    input pcWrite;
+    output reg [WIDTH - 1 : 0] PC, nextPC;
 
     ALUandRF alurf(
         .clk(clk), 
         .reset(reset),
 	input [WIDTH - 1 : 0] pc, immd,
 	input [3 : 0] srcAddr, dstAddr,
-	input pcInstruction, rTypeInstruction, shiftInstruction, regWrite, flagSet, copyInstruction,
-	input [2:0] aluOp,
+	    .pcInstruction(pcInstruction), 
+        .rTypeInstruction(rTypeInstruction), 
+        .shiftInstruction(shiftInstruction), 
+        .regWrite(regWrite), 
+        .flagSet(flagSet), 
+        .copyInstruction(copyInstruction),
+	    .aluOp(aluOp),
 	output reg [WIDTH - 1 : 0] resultData,
-	output wire [WIDTH - 1 : 0] outputFlags)
+	output wire [WIDTH - 1 : 0] outputFlags
+    );
+
+    // Flip-flop for the PC
+    always @(posedge clk) begin
+        if (~reset) PC <= 16'd0;
+        else if (pcWrite) PC <= nextPC;
+    end
+
+    // MUX for the next PC instruction
+    always @(*) begin
+        if (~reset) nextPC <= PC + 1;
+        else if (pcInstruction) nextPC <= resultData;
+        else nextPC <= PC + 1;
+    end
+endmodule
