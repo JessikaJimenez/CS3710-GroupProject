24dd19cb1bd5e42904067ad79128a4237bcddfb3
battl <battlecast33@gmail.com>
2022-10-18 00:51:12 +0000

    more tests

--- ALUandRF/tb_ALU.v
+++ ALUandRF/tb_ALU.v
@@ -93,12 +93,27 @@ module tb_ALU #(parameter WIDTH = 16) ();
 	    else $display("6. something is wrong");
 		 
 		 //Tests to see if zero flag is set to 1 when the result is zero
-		 aluOp <= 3'b100;
+		aluOp <= 3'b100;
 	    regDst <= 16'b1111111111111110;
 	    regSrc <= 16'b1111111111111110;
 	    #10
 	    if (negative==0 && carry==0 && zero==1 && low==0 && flag==0) $display("zero flag is correct");
 	    else $display("7. something is wrong");
+
+		aluOp <= 3'b000;
+	    regDst <= 16'b1111111111111110;
+	    regSrc <= 16'b1111111111111111;
+	    #10
+	    if (negative==1 && carry==1 && zero==0 && low==1 && flag==0) $display("carry flag is correct for unsigned addition");
+	    else $display("8. something is wrong");
+
+		//Testing to see if 3 + -3 sets zero flag and other flags correctly.
+		aluOp <= 3'b000;
+	    regDst <= 16'b0000000000000011;
+	    regSrc <= 16'b1111111111111101;
+	    #10
+	    if (negative==0 && carry==1 && zero==1 && low==1 && flag==0) $display("carry/flag/zero flags are correct");
+	    else $display("9. something is wrong");
 	end
 	
 endmodule 
