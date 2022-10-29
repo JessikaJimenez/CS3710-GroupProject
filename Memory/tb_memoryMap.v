90890ce3294a7dae4bd4d6ae58f1c99596de45d2
RolandoQuintanaTPG <107889590+RolandoQuintanaTPG@users.noreply.github.com>
2022-10-29 11:52:36 -0600

    updated and finished tb for memory map

--- Memory/tb_memoryMap.v
+++ Memory/tb_memoryMap.v
@@ -8,7 +8,7 @@ module tb_memoryMap #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) ();
   reg [(ADDR_WIDTH-1):0] addr_a, addr_b;
   reg write_a, write_b, clk;
   reg [(ADDR_WIDTH-1):0] InputData;
-  wire [(DATA_WIDTH-1):0] OutputDataA, OutputDataB;
+  wire [(DATA_WIDTH-1):0] OutputReadDataA, OutputWriteDataA, OutputReadDataB, OutputWriteDataB;
   integer i;
 
   // Instantiate memoryMap module
@@ -21,14 +21,21 @@ module tb_memoryMap #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) ();
     .write_b(write_b),
     .clk(clk),
     .InputData(InputData),
-    .OutputDataA(OutputDataA),
-    .OutputDataB(OutputDataB)
+    .OutputReadDataA(OutputReadDataA),
+	 .OutputWriteDataA(OutputWriteDataA),
+    .OutputReadDataB(OutputReadDataB),
+	 .OutputWriteDataB(OutputWriteDataB)
   );
 
-  always begin
-	  #5 
-    clk = ~clk;
-  end
+	// Start clock
+	initial begin
+	   clk <= 0;
+	end
+		
+	// Generate clock
+	always #10 begin
+	   clk = ~clk;
+	end
     
   initial begin
     data_a = 16'b0;
@@ -37,45 +44,58 @@ module tb_memoryMap #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) ();
     addr_b = 16'b0;
     write_a = 1'b0;
     write_b = 1'b0;
-    clk = 1'b0;
-    InputData = 8'b0;
+    InputData = 16'b0;
 
     #10;
 	  write_a = 1'b1;
 	  write_b = 1'b0;
     
-    for(i = 0; i < 32; i = i+1) begin
+    for(i = 0; i < 16; i = i+1) begin
       data_a = i;
       addr_a = i;
-      #10;
+      #20;
 	  end
 
-  	addr_a = 16'b0;
+  	  addr_a = 16'b0;
 	  addr_b = 16'b0;
 	  write_a = 1'b0;
-  	write_b = 1'b1;
+  	  write_b = 1'b1;
   	#10;
     
-    for(i = 32; i < 64; i = i+1) begin
-      data_a = i;
-      addr_a = i;
-      #10;
+    for(i = 16; i < 32; i = i+1) begin
+      data_b = i;
+      addr_b = i;
+      #20;
 	  end
     
-    addr_a = 16'b0;
+   addr_a = 16'b0;
   	addr_b = 16'b0;
   	write_a = 1'b0;
-  	write_b = 1'b0;	
+  	write_b = 1'b0;
   	#10;
     
-    for(i = 510; i < 514; i = i+1) begin
+	 //READ FROM IO
+	 $display("Reading from IO");
+    for(i = 510; i < 515; i = i+1) begin
+	   InputData = (i-509);
       addr_a = i;
       addr_b = i;
-      #10;
+		#10;
+		$display("Value of input Data is: %d Read From Output Read A is: %d   Read From OutPut Read B is: %d", InputData, OutputReadDataA, OutputReadDataB);
+      
     end
 	 
+	 //WRITE TO IO
+	 $display("Write to IO");
+	 write_a = 1'b1;
+	 for(i = 510; i < 515; i = i+1) begin
+      addr_a = i;
+		data_a = (i-509);
+		#20;
+		$display("Value of Write Data is: %d The output of OutputWriteDataA is: %d", data_a, OutputWriteDataA);
+      
+    end
    #100;
-	  $finish;
     
   end
 
