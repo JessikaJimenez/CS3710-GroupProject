ce29739f7f7061bf5243ef1b8645054790925627
RolandoQuintanaTPG <107889590+RolandoQuintanaTPG@users.noreply.github.com>
2022-10-29 11:52:52 -0600

    finished tb for memory FSM

--- Memory/tb_memoryFSM.v
+++ Memory/tb_memoryFSM.v
@@ -1,14 +1,13 @@
 // TESTBENCH FOR memoryMap FILE
 /*************************************************************/
-`timescale 1ns / 1ps
+`timescale 1ns/1ns
 
-module tb_memoryFSM #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) 
-();
+module tb_memoryFSM #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) ();
   reg clk, rst, btn;
   reg [(ADDR_WIDTH-1):0] inAddress;
-  reg [(ADDR_WIDTH-1):0] outAddress;
-  reg [(DATA_WIDTH-1):0] writeData;
-  reg [(ADDR_WIDTH-1):0] readData;
+  wire [(ADDR_WIDTH-1):0] outAddress;
+  wire [(DATA_WIDTH-1):0] writeData;
+  wire [(ADDR_WIDTH-1):0] readData;
 
   // Instantiate memoryMap module
   memoryFSM mFSM (
@@ -17,14 +16,14 @@ module tb_memoryFSM #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16)
     .btn(btn),
     .inAddress(inAddress),
     .outAddress(outAddress),
-    .writeData(writeData),
-    .readData(readData),
+    .hexWriteData(writeData),
+    .hexReadData(readData)
   );
 
 	// Start clock and reset
 	initial begin
 	   clk <= 0;
-	   reset <= 1;
+	   rst <= 1;
 	end
 		
 	// Generate clock
@@ -33,8 +32,43 @@ module tb_memoryFSM #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16)
 	end
     
   initial begin
+    #20
     inAddress <= 16'd2;
+	 #15
     $display("Value at address %d is: %d", inAddress, readData);
+	 //Read State
+	 btn <= 1'b0;
+	 #10
+	 btn <= 1'b1;
+	 #10
+	 //Write State
+	 btn <= 1'b0;
+	 #10
+	 btn <= 1'b1;
+	 #10;
+	 //Complete Write and Enter Read State
+	 btn <= 1'b0;
+	 #10
+	 btn <= 1'b1;
+	 #30;
+	 $display("New Value at address %d is: %d", inAddress, readData);
+	 
+	 //Change Address and Repeat
+	 #10
+    inAddress <= 16'd3;
+	 #15
+    $display("Value at address %d is: %d", inAddress, readData);
+	 //Write State
+	 btn <= 1'b0;
+	 #10
+	 btn <= 1'b1;
+	 #10;
+	 //Complete Write and Enter Read State
+	 btn <= 1'b0;
+	 #10
+	 btn <= 1'b1;
+	 #30;
+	 $display("New Value at address %d is: %d", inAddress, readData);
   end
 
 endmodule 
