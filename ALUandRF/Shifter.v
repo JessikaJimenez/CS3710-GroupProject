91fdcb272d171f3c245de0df6e9b6e26ae90dfb4
Nami Eskandarian <u1117098@utah.edu>
2022-11-15 17:33:25 -0700

    Fixed shifter

--- ALUandRF/Shifter.v
+++ ALUandRF/Shifter.v
@@ -10,10 +10,14 @@ module Shifter #(parameter INPUTWIDTH = 16, parameter OUTPUTWIDTH = 16, paramete
     input logicalShift; // Flag for determining logical or arithmetic shift
     output reg [OUTPUTWIDTH - 1 : 0] shiftResult;
 
+    // We will need to invert shift amount if it is negative
+    wire absoluteShiftAmount;
+    assign absoluteShiftAmount = -shiftAmount;
+
     always @(*) begin
         if (!reset) shiftResult <= shiftInput;
         else if (rightShift) begin
-            shiftResult <= (shiftInput >> shiftAmount);
+            shiftResult <= (shiftInput >> absoluteShiftAmount);
             if (logicalShift) shiftResult[OUTPUTWIDTH - 1] <= 1'b0; // Zero extend
             else shiftResult[OUTPUTWIDTH - 1] <= shiftResult[OUTPUTWIDTH - 2]; // Sign extend
         end
