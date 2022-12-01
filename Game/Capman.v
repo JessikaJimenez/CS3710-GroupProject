3be197010f5d6885c06ce862a77c150151f84045
Jessikeks <jessikune@gmail.com>
2022-12-01 15:11:20 -0700

    Added instantiation for GeneralCPU, etc.

--- Game/Capman.v
+++ Game/Capman.v
@@ -2,47 +2,47 @@
 /* Top-level module for our game: Capman */
 /*************************************************************/
 module Capman (
-	input        clk,         //Onboard 50MHz clock
-	input        reset,       //Reset switch
-	input  [9:0] sw,          //Switches
-	output [9:0] LED,         //LEDs
-	// add other inputs/outputs here
+	input        clk,     //Onboard 50MHz clock
+	input        reset,   //Active-low reset
+	//input  [9:0] sw,      //Switches
+	output [7:0] LED,     //LEDs
+	output [6:0] hexOut,  //HEX-to-7-seg
+	output [7:0] Red,     //VGA red
+	output [7:0] Green,   //VGA green
+	output [7:0] Blue,    //VGA blue
 	
-	// stuff for VGA
-	output       VGA_CLK,     //VGA clock
-	output       VGA_HS,      //VGA horizontal sync 
-	output       VGA_VS,      //VGA vertical sync
-	output       VGA_BLANK_N, //VGA BLANK
-	output       VGA_SYNC_N,  //VGA N SYNC
-	output [7:0] VGA_R,       //VGA Red[7:0]
-	output [7:0] VGA_G,       //VGA Green[7:0]
-	output [7:0] VGA_B        //VGA Blue[7:0]
 );
 	
-	//**TODO - do game stuff
+	//Variables for CPU
+	wire [15:0] memData;
+	wire [15:0] addr;
+	wire [15:0] IOinput;
+	wire [15:0] writeEnable;
+	wire [15:0] memOutput;
+	wire [15:0] IOoutput;
+	//Variables for NES controller
+	wire nesData;
+    	wire nesClock;
+    	wire nesLatch;
+	wire [6:0] hexOut;
+	//Variables for VGA controller
+	wire [15:0] read_b;	
+	wire [15:0] addr_b;
+	wire hSync, vSync, splitClk, bright, sync_n;
+		
 	
-	// Read in game logic assembly file
-	initial begin
-		$display("Loading CapMan game from memory");
-		// **TODO - fix file path 
-		$readmemb("C:/Users/sizzl/OneDrive/Documents/School Documents/CS 3710/Project/CS3710-GroupProject/Helper Files/TestMem.dat", ram); 
-		$display("Done with memory load"); 
-	end	
-
-	//Instantiate VGA Controller
-	VGAController VGA(
-	        .clk(clk),
-		.reset(reset),
-		.VGA_CLK(VGA_CLK),
-		.VGA_HS(VGA_HS),
-		.VGA_VS(VGA_VS),
-		.VGA_BLANK_N(VGA_BLANK_N),
-		.VGA_SYNC_N(VGA_SYNC_N)),
-		.VGA_R(VGA_R),
-		.VGA_G(VGA_G),
-		.VGA_B(VGA_B)
-		//**TODO - actually make VGA controller
+	//Instantiate CPU 
+	GeneralCPU CPU (
+		.clk(clk),
+		.reset(reset),	
+		.memData(memData),
+		.addr(addr),
+		.IOinput(IOinput),
+		.writeEnable(writeEnable),
+		.memOutput(memOutput),
+		.IOoutput(IOoutput)
 	);
+	
 	
 	//Instantiate NES Controller
 	nesOnBoard NES (
@@ -50,8 +50,25 @@ module Capman (
 		.nesData(nesData),
 		.nesClock(nesClock),
 		.nesLatch(nesLatch),
-		.leds(leds),
+		.leds(LEDS),
 		.hexOut(hexOut)
+	);
+	
+	
+	//Instantiate VGA Controller
+	vgaDisplay VGA(
+	        .clk(clk),
+		.clear(reset),
+		.read_b(read_b),
+		.addr_b(addr_b),
+		.hSync(hSync),
+		.vSync(vSync),
+		.splitClk(splitClk),
+		.bright(bright),
+		.sync_n(sync_n),
+		.Red(Red),
+		.Green(Green),
+		.Blue(Blue)
 	);
 		
 
