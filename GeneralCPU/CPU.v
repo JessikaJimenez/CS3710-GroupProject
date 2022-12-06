6b648d83afabe38cb3aa58a76a8befbe6c936b14
Jessikeks <jessikune@gmail.com>
2022-12-06 02:27:38 -0700

    Update CPU.v

--- GeneralCPU/CPU.v
+++ GeneralCPU/CPU.v
@@ -6,20 +6,16 @@ module CPU #(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=16) (
 	output [9:0] leds   //LEDs LEDR9-LEDR0
 );
 
-	reg [(ADDR_WIDTH-1):0] memData;
 	wire [(ADDR_WIDTH-1):0] memOutput;
-	reg [(ADDR_WIDTH-1):0] addr;
-	wire writeEnable;
 
-	
 	// Instantiate modules
 	GeneralCPU #(.ADDR_WIDTH(13)) cpu (
  	   .clk(clk), 
 	   .reset(reset),
-	   .memData(memData),
-	   .addr(addr), 
+	   .memData(12'b0),
+	   .addr(12'b0), 
 	   .IOinput(sw), 
-	   .writeEnable(writeEnable), 
+	   .writeEnable(1'b0), 
 	   .memOutput(memOutput),
 	   .IOoutput(leds) 
 	);
